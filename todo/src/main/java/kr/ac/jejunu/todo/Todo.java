5ad5f5d2f62f8ffb79da00bd709782202447a8f6
Yongjae <k33721@gmail.com>
2020-06-26 03:43:46 +0900

    POST todo 투두 저장

--- todo/src/main/java/kr/ac/jejunu/todo/Todo.java
+++ todo/src/main/java/kr/ac/jejunu/todo/Todo.java
@@ -1,6 +1,10 @@
 package kr.ac.jejunu.todo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,10 +13,15 @@ import javax.persistence.Id;
 
 @Entity(name = "todo")
 @Data
+@DynamicInsert
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String task;
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Boolean is_done;
 }
--- todo/src/main/java/kr/ac/jejunu/todo/TodoController.java
+++ todo/src/main/java/kr/ac/jejunu/todo/TodoController.java
@@ -2,6 +2,8 @@ package kr.ac.jejunu.todo;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,5 +16,10 @@ public class TodoController {
     @GetMapping("/todo")
     public List<Todo> get() {
         return todoDao.findAll();
+    }
+
+    @PostMapping("/save")
+    public Todo create(@RequestBody Todo todo){
+        return todoDao.save(todo);
     }
 }
