a4b081768eb364ac50e2fb89ba24377f5bd05998
Yongjae <k33721@gmail.com>
2020-06-26 04:39:47 +0900

    같은 URL(save/) POST, PUT 두개 하나로 합치기, 투두 상위 URL변경

--- todo/src/main/java/kr/ac/jejunu/todo/TodoController.java
+++ todo/src/main/java/kr/ac/jejunu/todo/TodoController.java
@@ -6,22 +6,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/todo")
 @RequiredArgsConstructor
 public class TodoController {
     private final TodoDao todoDao;
 
-    @GetMapping("/todo")
+    @GetMapping()
     public List<Todo> get() {
         return todoDao.findAll();
     }
 
-    @PostMapping("/save")
+    @RequestMapping(path = "/save", method = {RequestMethod.POST, RequestMethod.PUT})
     public Todo create(@RequestBody Todo todo){
-        return todoDao.save(todo);
-    }
-
-    @PutMapping("/save")
-    public Todo modify(@RequestBody Todo todo){
         return todoDao.save(todo);
     }
 
